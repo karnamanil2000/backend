3e765149d35885837d06c737abacc34a96f40ea4
Anil <karnamanil2000@gmail.com>
2023-12-02 14:27:40 +0530

    Integrated with Frontend without DB.

---
+++ src/main/java/com/example/backend/controller/EmployeeController.java
@@ -0,0 +1,25 @@
+package com.example.backend.controller;
+
+import com.example.backend.entity.Employee;
+import com.example.backend.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+@CrossOrigin("http://localhost:8081/")
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @GetMapping("/employees")
+    public List<Employee> fetchEmployees() {
+        return employeeRepository.findAll();
+    }
+}
